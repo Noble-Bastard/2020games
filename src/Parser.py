6987cfd193b886197d9ae5bd0a9643c5ecc3599f
Drago <71710984+Noble-Bastard@users.noreply.github.com>
2021-05-23 21:59:29 +0600

    remove unused elements

--- src/Parser.py
+++ src/Parser.py
@@ -4,18 +4,13 @@ import config
 
 
 def parseInit(url, header):
-   URL = config.SITE_URL
-   HEADERS = {
-       'User-Agent': config.USER_AGENT
-   }
    return requests.get(url, header)
 
 def parse(response):
    soup =  BeautifulSoup(response.content, 'lxml')
    items = soup.select('div.game-card')
    elements = []
-  # print(items)
- #  print(items.find_all('div'))
+
    for item in items:
       elements.append(
          {
@@ -26,10 +21,6 @@ def parse(response):
       )
    return elements
 
-
-  # print(elements)
-   # for element in elements:
-   #       print(element['game'])
 response = parseInit(config.SITE_URL, {'User-Agent': config.USER_AGENT})
 
 da = parse(response)
