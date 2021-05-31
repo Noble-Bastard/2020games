e3fa834466004f9071548cb072894705039dc137
Drago <71710984+Noble-Bastard@users.noreply.github.com>
2021-05-31 17:33:14 +0600

    Update ThreadsStart.py

--- src/ThreadsStart.py
+++ src/ThreadsStart.py
@@ -1,32 +1,31 @@
 from threading import Thread
-# from Setter import Setter
-# from Parser import Parser
-# import sqlite3
-# from time import time
-# import os
 
 class ThreadStart:
 
    def __init__(self, url):
       self.url = url
 
-
-   def startParsing(self, parserClass, parserClass1, parserClass2, parserClass3):
-      parserClass.parser(1, 10, self.url)
+   def startParsing(self, parserClass, parserClass1, parserClass2, parserClass3, parserClass4, header):
+      parserClass.parser(1, 20, self.url, header)
       thread = Thread(target=parserClass1.parser,
-                      args=(50, 99, self.url,))
+                      args=(241, 440, self.url, header,))
       thread1 = Thread(target=parserClass2.parser,
-                       args=(100, 149, self.url,))
+                       args=(441, 640, self.url, header,))
       thread2 = Thread(target=parserClass3.parser,
-                       args=(150, 200, self.url,))
+                       args=(641, 840, self.url, header,))
+      thread3 = Thread(target=parserClass4.parser,
+                       args=(841, 1040, self.url, header,))
+      thread4 = Thread(target=parserClass4.parser,
+                       args=(1041, 1348, self.url, header,))
 
       thread.start()
       thread1.start()
       thread2.start()
-      parserClass.parser(11, 49, self.url)
+      thread3.start()
+      thread4.start()
+      parserClass.parser(21, 240, self.url, header)
       thread.join()
       thread1.join()
       thread2.join()
-
-
-
+      thread3.join()
+      thread4.join()
