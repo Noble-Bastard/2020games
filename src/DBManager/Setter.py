7e222a5ae679652f9ae4040775c1bd9013e54140
Drago <71710984+Noble-Bastard@users.noreply.github.com>
2021-05-31 17:33:52 +0600

    Update Setter.py

--- src/DBManager/Setter.py
+++ src/DBManager/Setter.py
@@ -1,7 +1,18 @@
+import sqlite3
+
 class Setter:
 
-   def setGameInfo(self, games, database):
-      for game in games:
-        database.cursor().execute(
-          "INSERT INTO games VALUES (NULL, ?, ?, ?, ?, ?, ?)", (game['name'], game['link'], game['imageLink'], game['rating'], game['metacritic'], game['category']))
-      database.commit()
+
+   def setGameInfo(self, game):
+      games = True
+      while games == True:
+        try:
+            database = sqlite3.connect('game.db', isolation_level=None)
+            database.cursor().execute(
+                "INSERT INTO games VALUES (NULL, ?, ?, ?, ?, ?, ?)", (game['name'], game['link'], game['imageLink'], game['rating'], game['metacritic'], game['category']))
+            database.commit()
+            database.close()
+            break
+        except Exception as e:
+            print(e)
+            return self.setGameInfo(game)
